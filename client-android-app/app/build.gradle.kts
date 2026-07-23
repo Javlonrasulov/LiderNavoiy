d20b8a3d704b7bb8211c94e113a769dff492f5b1
Javlo <javlo@example.com>
2026-07-23 23:20:20 +0500

    Mijoz APK v1.0.1

--- client-android-app/app/build.gradle.kts
+++ client-android-app/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "uz.lider.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
